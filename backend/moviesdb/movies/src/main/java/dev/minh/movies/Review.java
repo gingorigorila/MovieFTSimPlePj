edf4a6d59903277160a24f9326bdce89e71231a5
gingorigorila <20521598@gm.uit.edu.vn>
2024-03-21 21:42:20 +0700

    Add MovieSimplePj

---
+++ backend/moviesdb/movies/src/main/java/dev/minh/movies/Review.java
@@ -0,0 +1,22 @@
+package dev.minh.movies;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="review")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    private ObjectId Id;
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
+}
